3ad84c71bbf3f1667f1604d51e7ea46cfc195369
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-05-29 12:22:50 +0200

    start

--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-let controlFont = document.getElementById('font-size-control');
-
-let controlText = document.querySelector('#text');
-
-controlFont.oninput = () => {
-  controlText.style.fontSize = controlFont.value + 'px';
-};
